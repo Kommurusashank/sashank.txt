4233f7766d2d143afdcc89ac672ce6111da03924
kommuru sashank <kommurusashank20@gmail.com>
2025-07-01 10:42:22 +0530

    Revert "Update python max.py"

--- python max.py
+++ python max.py
@@ -1,10 +1,6 @@
 a=[1,3,5,6,7,2]
-max1=a[0]
+max=a[0]
 for i in range (len(a)):
-    if(a[i]>max1):
-        max1=a[i]
-second max=a[0]
-for i in range (len(a)):
-    if a[i]!=max1 and a[i]>second max:
-       second max=a[i]
-print(second max)
+    if(a[i]>max):
+        max=a[i]
+print(max)
